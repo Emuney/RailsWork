0d78144da39e1f09f86980f6f4d21eec1c160830
Evan Munemura <emunemur@uci.edu>
2014-06-04 17:33:36 -0700

    update profile functions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -107,11 +107,6 @@ describe User do
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
-
-      describe "followed by signout" do
-        before { click_link "Sign out" }
-        it { should have_link('Sign in') }
-      end
     end
 
     describe "with invalid password" do
